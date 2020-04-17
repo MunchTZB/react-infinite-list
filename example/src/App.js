0ce48c0b6fcb4565e412640dd7bee1d0c8c4094a
tangzhenbang <tangzhenbang@bytedance.com>
2020-04-17 16:16:10 +0800

    update

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,21 @@
 import React from 'react'
 
-import { ExampleComponent } from 'react-infinite-list'
+import { InfiniteList } from 'react-infinite-list'
 import 'react-infinite-list/dist/index.css'
 
+const dataSource = new Array(1000000).fill(null).map((item, index) => `${index}: Lorem Ipsum`);
+// console.log(dataSource)
+
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  return <InfiniteList
+  dataSource={dataSource}
+  cellHeight={200}
+  render={(data) => data}
+  style={{
+    border: '1px solid #000',
+    height: 400,
+    overflowY: 'scroll'
+  }} />
 }
 
 export default App
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,138 @@
 import * as React from 'react'
-import styles from './styles.module.css'
+// import styles from './styles.module.css'
 
-interface Props {
-  text: string
+interface Props<T>
+  extends Pick<
+    React.DetailedHTMLProps<
+      React.HTMLAttributes<HTMLDivElement>,
+      HTMLDivElement
+    >,
+    'className' | 'style'
+  > {
+  dataSource: T[]
+  cellHeight: number | ((data: T, index: number) => number)
+  render: (data: T, index: number) => React.ReactElement
 }
 
-export const ExampleComponent = ({ text }: Props) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+interface State {
+  currentIndex: number,
+  cellPosition: number[]
+}
+
+export class InfiniteList<T extends any> extends React.Component<
+  Props<T>,
+  State
+> {
+  constructor(props: Props<T>) {
+    super(props)
+    this.state = {
+      currentIndex: 0,
+      cellPosition: [],
+    }
+  }
+  wrapperRef = React.createRef<HTMLDivElement>()
+  componentDidMount() {
+    this.calcPosition()
+  }
+
+  calcPosition() {
+    const { cellHeight, dataSource } = this.props
+    if (typeof cellHeight === 'number') {
+      this.setState({
+        cellPosition: dataSource.map((item, index) => {
+          return index * cellHeight
+        })
+      })
+    } else {
+      this.setState({
+        cellPosition:dataSource.map((item, index) => {
+          return cellHeight(item, index)
+        })
+      })
+    }
+  }
+
+  getTotalHeight() {
+    const {cellHeight, dataSource} = this.props;
+    const last = this.state.cellPosition.length - 1;
+    if (typeof cellHeight === 'number') {
+      return this.state.cellPosition[last] + cellHeight
+    } else {
+      return this.state.cellPosition[last] + cellHeight(dataSource[last], last)
+    }
+  }
+
+  onScroll = (event: React.UIEvent<HTMLDivElement, UIEvent>) => {
+    const { scrollTop } = event.currentTarget
+    // this.cellPosition.findIndex(())
+    let offsetIndex = 0
+    console.time('findIndex')
+    for (let i = 0; i < this.state.cellPosition.length; i++) {
+      if (scrollTop < this.state.cellPosition[i]) {
+        offsetIndex = i - 1
+        break
+      }
+    }
+    console.timeEnd('findIndex')
+    this.setState({
+      currentIndex: offsetIndex,
+    })
+  }
+
+  render() {
+    const { props } = this
+    const { currentIndex } = this.state
+    const renderIndexes = [
+      currentIndex - 1,
+      currentIndex,
+      currentIndex + 1,
+      currentIndex + 2,
+      currentIndex + 3,
+    ]
+
+    return (
+      <div
+        ref={this.wrapperRef}
+        className={props.className}
+        onScroll={this.onScroll}
+        style={props.style}
+      >
+        <div style={{
+          position: 'relative',
+          height: this.getTotalHeight()
+        }}>
+          {
+            renderIndexes.map(index => {
+              if ((this.state.cellPosition[index] === undefined)) {
+                return null
+              }
+              return (
+                <div key={index} style={{
+                  position: 'absolute',
+                  transform: `translateY(${this.state.cellPosition[index]}px)`,
+                  // @ts-ignore
+                  height: props.cellHeight,
+                }}>
+                  {props.render(props.dataSource[index], index)}
+                </div>
+              )
+            })
+          }
+          {/* {props.dataSource.map((cellData, index) => {
+            return (
+              <div
+                key={index}
+                style={{
+                  // @ts-ignore
+                  height: props.cellHeight,
+                }}
+              >
+                {props.render(cellData, index)}
+              </div>
+            )
+          })} */}
+        </div>
+      </div>
+    )
+  }
 }
